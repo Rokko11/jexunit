90bb4e4e567dc7ab2dcc341167b7c3e447227e54
Richard V <rokko11@users.noreply.github.com>
2020-07-07 12:33:02 +0200

    Fix import

--- core/src/main/java/com/jexunit/core/dataprovider/ExcelLoader.java
+++ core/src/main/java/com/jexunit/core/dataprovider/ExcelLoader.java
@@ -6,10 +6,7 @@ import com.jexunit.core.model.TestCase;
 import com.jexunit.core.model.TestCell;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileNotFoundException;
