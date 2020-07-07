b9c3d0d00a0c4bcd5eea4f0cc047d76ccc1bee23
Richard V <rokko11@users.noreply.github.com>
2020-07-07 12:31:01 +0200

    Add some docu. BeforeSheet and AfterSheet allow throwing Exceptions

--- core/src/main/java/com/jexunit/core/spi/AfterSheet.java
+++ core/src/main/java/com/jexunit/core/spi/AfterSheet.java
@@ -1,5 +1,10 @@
 package com.jexunit.core.spi;
 
+/**
+ * Implementations of this interface can be called after the execution of a Excel file.
+ * It will get activated by {@link com.jexunit.core.JExUnitConfig.ConfigKey} jexunit.sheet.after
+ */
 public interface AfterSheet {
-    void run();
+
+    void run() throws Exception;
 }
--- core/src/main/java/com/jexunit/core/spi/BeforeSheet.java
+++ core/src/main/java/com/jexunit/core/spi/BeforeSheet.java
@@ -1,6 +1,10 @@
 package com.jexunit.core.spi;
 
+/**
+ * Implementations of this interface can be called before the execution of a Excel file.
+ * It will get activated by {@link com.jexunit.core.JExUnitConfig.ConfigKey} jexunit.sheet.after
+ */
 public interface BeforeSheet {
 
-    void run();
+    void run() throws Exception;
 }
